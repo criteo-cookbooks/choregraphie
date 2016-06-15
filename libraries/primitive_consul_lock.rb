6fdbc1b75bd2beee79dcbdb22b8ab7c99e6704b1
Grégoire Seux <g.seux@criteo.com>
2016-06-15 15:30:49 +0200

    Correct a bad call to a non-existing variable

--- libraries/primitive_consul_lock.rb
+++ libraries/primitive_consul_lock.rb
@@ -108,7 +108,7 @@ module Choregraphie
         Chef::Log.debug("Someone updated the lock at the same time, will retry") unless result
         result
       else
-        Chef::Log.error("#{@options[:id]} was not holding the lock. This is very wrong or someone removed it from lock")
+        Chef::Log.error("#{name} was not holding the lock. This is very wrong or someone removed it from lock")
         true
       end
     end
