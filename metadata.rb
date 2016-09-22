ffb8d19673c3a40daad748bb48bdfd6961b3ae0e
Grégoire Seux <g.seux@criteo.com>
2016-09-22 13:14:55 +0200

    Fix supermarket release from travis
    Change-Id: I4b3082a89e3cbd4065e48f7bd779d4da2d6675d0

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'g.seux@criteo.com'
 license          'Apache License'
 description      'Allow to coordinate changes induced by chef'
 long_description 'Installs/Configures choregraphie'
-issues_url       'https://github.com/criteo-cookbooks/choregraphie'
-source_url       'https://github.com/criteo-cookbooks/choregraphie'
+issues_url       'https://github.com/criteo-cookbooks/choregraphie' if respond_to?(:issues_url)
+source_url       'https://github.com/criteo-cookbooks/choregraphie' if respond_to?(:source_url)
 version          '0.4.2'
 supports         'centos'
 supports         'windows'
