b519be4eec35c168e4e473c5e87c9213d6492a80
Grégoire Seux <g.seux@criteo.com>
2016-11-07 17:27:33 +0100

    Re-enable kitchen tests
    Many changes just to make them pass
    
    Change-Id: I1fef2bc00934630c0ea8a0fe432b77c85e5a5631

--- spec/unit/metadata/default_spec.rb
+++ spec/unit/metadata/default_spec.rb
@@ -8,18 +8,17 @@
 require 'spec_helper'
 
 describe 'choregraphie::metadata' do
-
   let(:metadata_file) do
     File.join(File.dirname(__FILE__), '../../../metadata.rb')
   end
 
   it 'has a metadata file' do
-    expect(File.exists?(metadata_file))
+    expect(File.exist?(metadata_file))
   end
 
   it 'is parsable metadata' do
     m = Chef::Cookbook::Metadata.new
-    expect{ m.from_file(metadata_file) }.not_to raise_error
+    expect { m.from_file(metadata_file) }.not_to raise_error
   end
 
   it 'has a license' do
