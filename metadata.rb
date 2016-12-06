ad8b65b99e74395f1e304103e82739dd7189f97e
Grégoire Seux <g.seux@criteo.com>
2016-12-06 13:57:31 +0100

    Release 0.7.1
    Change-Id: I9254943605fcb80051999e806a710911216a8699

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'g.seux@criteo.com'
 license          'Apache License'
 description      'Allow to coordinate changes induced by chef'
 long_description 'Installs/Configures choregraphie'
-issues_url       'https://github.com/criteo-cookbooks/choregraphie'
-source_url       'https://github.com/criteo-cookbooks/choregraphie'
-version          '0.7.0'
+issues_url       'https://github.com/criteo-cookbooks/choregraphie' if respond_to? :issues_url
+source_url       'https://github.com/criteo-cookbooks/choregraphie' if respond_to? :source_url
+version          '0.7.1'
 supports         'centos'
 supports         'windows'
 
