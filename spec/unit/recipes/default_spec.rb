14232c88766b957caa75749dbebe1fe8964dfef8
Grégoire Seux <g.seux@criteo.com>
2018-08-31 16:05:04 +0200

    Update unit tests fauxhai versions
    Let's drop centos6, update centos7 version and update windows version.
    
    Change-Id: I9693085871822ee223786545fba13518c3aabf8f

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe 'test::default' do
-  context 'When all attributes are default, on centos 6.7' do
+  context 'When all attributes are default, on centos 7.5.1804' do
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new(
         platform: 'centos',
-        version:  '6.7'
+        version:  '7.5.1804'
       )
       stub_command("test -f /tmp/not_converging.tmp").and_return(0)
       runner.converge(described_recipe)
@@ -15,25 +15,11 @@ describe 'test::default' do
       expect { chef_run }.to_not raise_error
     end
   end
-  context 'When all attributes are default, on centos 7.2.1511' do
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new(
-        platform: 'centos',
-        version:  '7.2.1511'
-      )
-      stub_command("test -f /tmp/not_converging.tmp").and_return(0)
-      runner.converge(described_recipe)
-    end
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-  end
-  context 'When all attributes are default, on windows 6.3.9600' do
+  context 'When all attributes are default, on windows 8.1' do
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new(
         platform: 'windows',
-        version:  '2008R2'
+        version:  '8.1'
       )
       stub_command("test -f /tmp/not_converging.tmp").and_return(0)
       runner.converge(described_recipe)
