88de8383bf9370ac369d2608c6d73ec87f0dd004
Grégoire Seux <g.seux@criteo.com>
2019-06-06 08:54:48 +0200

    Release 0.15.4
    Change-Id: If38414ee2ef62519c863bc63b16094f736700e9f

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Coordinates the application of changes induced by chef'
 long_description 'Installs/Configures choregraphie'
 issues_url       'https://github.com/criteo-cookbooks/choregraphie' if respond_to? :issues_url
 source_url       'https://github.com/criteo-cookbooks/choregraphie' if respond_to? :source_url
-version          '0.15.3'
+version          '0.15.4'
 supports         'centos'
 supports         'windows'
 
