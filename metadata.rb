4d415d1fc9b94aa4696119ac771f533be2b29f15
Jeremy Clerc <j.clerc@criteo.com>
2020-02-13 15:50:20 +0100

    Release 0.15.7
    - Do not use side effects to setup token when using diplomat > 2.1

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Coordinates the application of changes induced by chef'
 long_description 'Installs/Configures choregraphie'
 issues_url       'https://github.com/criteo-cookbooks/choregraphie' if respond_to? :issues_url
 source_url       'https://github.com/criteo-cookbooks/choregraphie' if respond_to? :source_url
-version          '0.15.6'
+version          '0.15.7'
 supports         'centos'
 supports         'windows'
 
