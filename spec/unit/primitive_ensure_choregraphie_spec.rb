b97d15a3a41cdf0dc934d7663d6b13d70036f436
Grégoire Seux <g.seux@criteo.com>
2021-11-05 12:14:54 +0100

    Refactor tests prior modification
    Tests were partially buggy (not checking the announced behavior, it used
    to work because of the bug fixed in previous commit).
    
    Change-Id: Ief095b8702f03cad6583be9aabbed6998c72b1fb

--- spec/unit/primitive_ensure_choregraphie_spec.rb
+++ spec/unit/primitive_ensure_choregraphie_spec.rb
@@ -3,7 +3,7 @@ require_relative '../../libraries/primitive_ensure_choregraphie'
 describe Choregraphie::EnsureChoregraphie do
   let(:choregraphie) do
     Choregraphie::Choregraphie.new('test') do
-      on 'test[name]'
+      on 'my_resource_to_protect[name]'
       ensure_choregraphie 'random', period: 0.01
     end
   end
@@ -11,25 +11,28 @@ describe Choregraphie::EnsureChoregraphie do
     expect_any_instance_of(Choregraphie::Choregraphie).to receive(:ruby_block)
   end
 
-  it 'must check file if resource is not protected' do
+  it 'must check file if resource is not protected by another choregraphie' do
     expect(Choregraphie::Choregraphie).to receive(:all).and_return(
       [
-        double(name: 'toto', resources: ['toto[name]'])
+        choregraphie,
+        double(name: 'toto', resources: ['another_unrelated_resource[name]']),
       ],
     )
     expect(File).to receive(:exist?).with('random').and_return(true)
-    choregraphie.before.each { |block| block.call 'test[name]' }
+    choregraphie.before.each { |block| block.call 'my_resource_to_protect[name]' }
   end
+
   it 'must not check file if resource is protected' do
     expect(Choregraphie::Choregraphie).to receive(:all).and_return(
       [
-        double(name: 'test', resources: ['test[name]']),
-        double(name: 'toto', resources: ['toto[name]'])
+        choregraphie,
+        double(name: 'toto', resources: ['my_resource_to_protect[name]']),
       ],
     )
     expect(File).to_not receive(:exist?).with('random')
-    choregraphie.before.each { |block| block.call 'test[name]' }
+    choregraphie.before.each { |block| block.call 'my_resource_to_protect[name]' }
   end
+
   it 'must clean the file in cleanup' do
     expect(File).to receive(:exist?).and_return(true)
     expect(FileUtils).to receive(:rm).with('random')
