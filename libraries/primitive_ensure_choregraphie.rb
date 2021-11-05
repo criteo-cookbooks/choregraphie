916bb18f36274a65ff95c253c51875d660f8fbe4
Flavien Quesnel <f.quesnel@criteo.com>
2021-11-05 11:47:10 +0100

    [Breaking] Fix ensure_choregraphie primitive
    ensure_choregraphie primitive now correctly blocks chef run when no real
    choregraphie is defined to protect a sensitive resource.
    
    The bug had always been there due to lack of exhaustive tests.
    
    Dirty details:
    
    It was due to write code such as:
    ```
    choregraphie 'toto' do
      on :weighted_resources
      ensure_choregraphie
    end
    ```
    
    which was executed in this (non obvious) order:
    
    - ensure_choregraphie->register method (which clears the list of resources)
    - on :weighted_resources -> list all resources and add them to the list called `choregraphie.resources` of ensure_choregraphie
    - when we execute Choregraphie.all.none?(...) the resource list is not
      empty which leads to think there is a choregraphie protecting the resources
    
    Change-Id: I0396276fef90a7f6e6b58d0b58ee57dac367d852
    Co-authored-by: Grégoire Seux <g.seux@criteo.com>
    Co-authored-by: Xavier Milliard <x.milliard@criteo.com>
    Co-authored-by: Flavien Quesnel <f.quesnel@criteo.com>

--- libraries/primitive_ensure_choregraphie.rb
+++ libraries/primitive_ensure_choregraphie.rb
@@ -9,14 +9,12 @@ module Choregraphie
 
 
     def register(choregraphie)
-      # We clear the resources list since we do not want other choregraphies
-      # to think that they are protected with this.
-      choregraphie.resources.clear
-
+      current_name = choregraphie.name
       choregraphie.before do |resource_name|
         raise 'ensure_choregraphie primitive cannot be used on other events than resource convergence!' unless resource_name
 
-        if ::Choregraphie::Choregraphie.all.none? { |c| c.resources.include?(resource_name) }
+        considered_choregraphies = ::Choregraphie::Choregraphie.all.reject { |c| c.name == current_name }
+        if considered_choregraphies.none? { |c| c.resources.include?(resource_name) }
           Chef::Log.warn "Resource #{resource_name} is about to converge but no choregraphie has been set up to protect this, please touch file #{@file_path} if you want to converge anyway."
           sleep(@period) until ::File.exists?(@file_path)
         end
